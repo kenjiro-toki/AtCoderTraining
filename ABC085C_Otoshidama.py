61e5c8727d2e3f419a91dc24f5c50c069cc5dc0d
kenjiro <hiraken@mbp.local>
2023-02-09 14:07:04 +0900

    高速化、AC

--- ABC085C_Otoshidama.py
+++ ABC085C_Otoshidama.py
@@ -1,23 +1,43 @@
 # 多重ループを抜ける方法
-# 処理が重すぎて没
+# 処理が重すぎた
 
 N, Y = map(int, input().split())
 
+# for x in range(N+1):
+#     for y in range(N+1):
+#         for z in range(N+1):
+#             sum = 10000*x + 5000*y + 1000*z
+#             total = x + y + z
+#             # print(total)
+#             if sum == Y and total == N:
+#                 print(f"{x} {y} {z}")
+#                 break
+#             elif x == y == z == N:
+#                 print("-1 -1 -1")
+#                 break
+#         else:
+#             continue
+#         break
+#     else:
+#         continue
+#     break
+
 for x in range(N+1):
     for y in range(N+1):
-        for z in range(N+1):
-            sum = 10000*x + 5000*y + 1000*z
-            total = x + y + z
-            # print(total)
-            if sum == Y and total == N:
-                print(f"{x} {y} {z}")
-                break
-            elif x == y == z == N:
+        z = N - (x + y)
+        sum = 10000*x + 5000*y + 1000*z
+        # print(total)
+        if z < 0:
+            # print(f"{x} {y} {z}")
+            if x == y == N:
                 print("-1 -1 -1")
                 break
-        else:
-            continue
-        break
+        elif sum == Y:
+            print(f"{x} {y} {z}")
+            break
+        elif x == y == N:
+            print("-1 -1 -1")
+            break
     else:
         continue
     break
